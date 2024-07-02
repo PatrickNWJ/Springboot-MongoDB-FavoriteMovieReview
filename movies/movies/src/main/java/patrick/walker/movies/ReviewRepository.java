3f31a0bed42a64e4b7a543294b4e724d19371336
PatrickNWJ <70920036+PatrickNWJ@users.noreply.github.com>
2024-07-02 13:52:19 -0700

    Add files via upload

---
+++ movies/movies/src/main/java/patrick/walker/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package patrick.walker.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
